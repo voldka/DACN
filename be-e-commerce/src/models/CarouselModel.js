29dba9b4491d1c13ff15a751ff85ae88fd4ec774
Nguyen Duc Hoa <nguyenduchoa0130@gmail.com>
2023-12-11 00:12:36 +0700

    Update code

--- be-e-commerce/src/models/CarouselModel.js
+++ be-e-commerce/src/models/CarouselModel.js
@@ -1,25 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CarouselSchema = new Schema(
   {
-    name: {
+    imageUrl: {
       type: Schema.Types.String,
       required: true,
     },
-    description: {
-      type: Schema.Types.String,
-      required: true,
-    },
-    image: {
-      type: Schema.Types.String,
+    order: {
+      type: Number,
       required: true,
     },
   },
   {
     timestamps: true,
-  }
+  },
 );
 
-const CarouselModel = mongoose.model("Carousels", CarouselSchema);
+const CarouselModel = mongoose.model('Carousels', CarouselSchema);
 module.exports = CarouselModel;
--- be-e-commerce/src/services/CarouselService.js
+++ be-e-commerce/src/services/CarouselService.js
@@ -1,337 +1,92 @@
 const CarouselModel = require('../models/CarouselModel');
 
-const createCarousel = (newCarousel) => {
+const SUCCESS = 'Thành Công';
+
+const getAll = () => {
   return new Promise(async (resolve, reject) => {
-    const { name, description, link } = newCarousel;
     try {
-      const checkCarousel = await CarouselModel.findOne({
-        name: name,
+      const carousels = await CarouselModel.find()
+        .select({ __v: 0, updatedAt: 0 })
+        .sort([
+          ['order', 'asc'],
+          ['createdAt', 'asc'],
+        ]);
+      return resolve({
+        status: 'success',
+        statusCode: 200,
+        message: SUCCESS,
+        data: carousels,
       });
-      if (checkCarousel !== null) {
-        resolve({
-          status: 'ERR',
-          message: 'Đã tồn tại giá trị trường name tương tự',
-          data: {
-            total: null,
-            pageCurrent: null,
-            totalPage: null,
-            userData: null,
-            productData: null,
-            orderData: null,
-            carouselData: null,
-            commentData: null,
-          },
-          access_token: null,
-          refresh_token: null,
-        });
-      }
-      const newCarousel = await CarouselModel.create({
-        name,
-        description,
-        link,
-      });
-      if (newCarousel) {
-        resolve({
-          status: 'OK',
-          message: 'Thành công',
-          data: {
-            total: null,
-            pageCurrent: null,
-            totalPage: null,
-            userData: null,
-            productData: null,
-            orderData: null,
-            carouselData: newCarousel,
-            commentData: null,
-          },
-          access_token: null,
-          refresh_token: null,
-        });
-      }
-    } catch (e) {
-      reject(e);
+    } catch (error) {
+      return reject(error);
     }
   });
 };
 
-const updateCarousel = (id, data) => {
+const create = (data) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const checkCarousel = await CarouselModel.findOne({
-        _id: id,
+      const carousel = await CarouselModel.create(data);
+      return resolve({
+        status: 'success',
+        statusCode: 201,
+        message: SUCCESS,
+        data: carousel,
       });
-      if (checkCarousel === null) {
-        resolve({
-          status: 'ERR',
-          message: 'Không tìm thấy',
-          data: {
-            total: null,
-            pageCurrent: null,
-            totalPage: null,
-            userData: null,
-            productData: null,
-            orderData: null,
-            carouselData: null,
-            commentData: null,
-          },
-          access_token: null,
-          refresh_token: null,
-        });
-      }
-
-      const updatedCarousel = await CarouselModel.findByIdAndUpdate(id, data, {
-        new: true,
-      });
-      resolve({
-        status: 'OK',
-        message: 'Thành công',
-        data: {
-          total: null,
-          pageCurrent: null,
-          totalPage: null,
-          userData: null,
-          productData: null,
-          orderData: null,
-          carouselData: updatedCarousel,
-          commentData: null,
-        },
-        access_token: null,
-        refresh_token: null,
-      });
-    } catch (e) {
-      reject(e);
+    } catch (error) {
+      return reject(error);
     }
   });
 };
 
-const deleteCarousel = (id) => {
+const update = (carouselId, changes) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const checkCarousel = await CarouselModel.findOne({
-        _id: id,
-      });
-      if (checkCarousel === null) {
-        resolve({
-          status: 'ERR',
-          message: 'Không tìm thấy',
-          data: {
-            total: null,
-            pageCurrent: null,
-            totalPage: null,
-            userData: null,
-            productData: null,
-            orderData: null,
-            carouselData: null,
-            commentData: null,
-          },
-          access_token: null,
-          refresh_token: null,
+      const carousel = await CarouselModel.findById(carouselId);
+      if (!carousel) {
+        return reject({
+          status: 'error',
+          statusCode: 404,
+          message: `Không tìm thấy banner có ID: ${carouselId}`,
+          data: carousel,
         });
       }
-
-      await CarouselModel.findByIdAndDelete(id);
-      resolve({
-        status: 'OK',
-        message: 'Xóa thành công',
-        data: {
-          total: null,
-          pageCurrent: null,
-          totalPage: null,
-          userData: null,
-          productData: null,
-          orderData: null,
-          carouselData: null,
-        },
-        access_token: null,
-        refresh_token: null,
+      const updatedCarousel = await CarouselModel.findByIdAndUpdate(carouselId, changes);
+      return resolve({
+        status: 'success',
+        statusCode: 200,
+        message: SUCCESS,
+        data: updatedCarousel,
       });
-    } catch (e) {
-      reject(e);
+    } catch (error) {
+      return reject(error);
     }
   });
 };
 
-const deleteManyCarousel = (ids) => {
+const remove = (carouselId) => {
   return new Promise(async (resolve, reject) => {
     try {
-      await CarouselModel.deleteMany({ _id: ids });
-      resolve({
-        status: 'OK',
-        message: 'Xóa thành công',
-        data: {
-          total: null,
-          pageCurrent: null,
-          totalPage: null,
-          userData: null,
-          productData: null,
-          orderData: null,
-          carouselData: null,
-          commentData: null,
-        },
-        access_token: null,
-        refresh_token: null,
-      });
-    } catch (e) {
-      reject(e);
+      const carousel = await CarouselModel.findById(carouselId);
+      if (!carousel) {
+        return reject({
+          status: 'error',
+          statusCode: 404,
+          message: `Không tìm thấy banner có ID: ${carouselId}`,
+          data: carousel,
+        });
+      }
+      await CarouselModel.deleteOne({ _id: carouselId });
+      return resolve(null);
+    } catch (error) {
+      return reject(error);
     }
   });
 };
-
-const getDetailsCarousel = (id) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const Carousel = await CarouselModel.findOne({
-        _id: id,
-      });
-      if (Carousel === null) {
-        resolve({
-          status: 'ERR',
-          message: 'Không tìm thấy',
-          data: {
-            total: null,
-            pageCurrent: null,
-            totalPage: null,
-            userData: null,
-            productData: null,
-            orderData: null,
-            carouselData: null,
-            commentData: null,
-          },
-          access_token: null,
-          refresh_token: null,
-        });
-      }
-      resolve({
-        status: 'OK',
-        message: 'Thành công',
-        data: {
-          total: null,
-          pageCurrent: null,
-          totalPage: null,
-          userData: null,
-          productData: null,
-          orderData: null,
-          carouselData: Carousel,
-          commentData: null,
-        },
-        access_token: null,
-        refresh_token: null,
-      });
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
-
-const getAllCarousel = (limit, page, sort, filter) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const totalCarousel = await CarouselModel.count();
-      let allCarousel = [];
-      if (filter) {
-        const label = filter[0];
-        const allObjectFilter = await CarouselModel.find({
-          [label]: { $regex: filter[1] },
-        })
-          .limit(limit)
-          .skip(page * limit)
-          .sort({ createdAt: -1, updatedAt: -1 });
-        resolve({
-          status: 'OK',
-          message: 'Thành công',
-          data: {
-            total: totalCarousel,
-            pageCurrent: Number(page + 1),
-            totalPage: Math.ceil(totalCarousel / limit),
-            userData: null,
-            productData: null,
-            orderData: null,
-            carouselData: allObjectFilter,
-            commentData: null,
-          },
-          access_token: null,
-          refresh_token: null,
-        });
-      }
-      if (sort) {
-        const objectSort = {};
-        objectSort[sort[1]] = sort[0];
-        const allCarouselSort = await CarouselModel.find()
-          .limit(limit)
-          .skip(page * limit)
-          .sort(objectSort)
-          .sort({ createdAt: -1, updatedAt: -1 });
-        resolve({
-          status: 'OK',
-          message: 'Thành công',
-          data: {
-            total: totalCarousel,
-            pageCurrent: Number(page + 1),
-            totalPage: Math.ceil(totalCarousel / limit),
-            userData: null,
-            productData: null,
-            orderData: null,
-            carouselData: allCarouselSort,
-            commentData: null,
-          },
-          access_token: null,
-          refresh_token: null,
-        });
-      }
-      if (!limit) {
-        allCarousel = await CarouselModel.find().sort({
-          createdAt: -1,
-          updatedAt: -1,
-        });
-      } else {
-        allCarousel = await CarouselModel.find()
-          .limit(limit)
-          .skip(page * limit)
-          .sort({ createdAt: -1, updatedAt: -1 });
-      }
-      resolve({
-        status: 'OK',
-        message: 'Thành công',
-        data: {
-          total: totalCarousel,
-          pageCurrent: Number(page + 1),
-          totalPage: Math.ceil(totalCarousel / limit),
-          userData: null,
-          productData: null,
-          orderData: null,
-          carouselData: allCarousel,
-          commentData: null,
-        },
-        access_token: null,
-        refresh_token: null,
-      });
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
-
-//   const getAllType = () => {
-//     return new Promise(async (resolve, reject) => {
-//       try {
-//         const allType = await CarouselModel.distinct("type");
-//         resolve({
-//           status: "OK",
-//           message: "Thành công",
-//           data: allType,
-//         });
-//       } catch (e) {
-//         reject(e);
-//       }
-//     });
-//   };
 
 module.exports = {
-  createCarousel,
-  updateCarousel,
-  getDetailsCarousel,
-  deleteCarousel,
-  getAllCarousel,
-  deleteManyCarousel,
-  // getAllType,
+  getAll,
+  create,
+  update,
+  remove,
 };
